3020c3eb4f6cdedf08c8e327a4e23785847c1d5f
Nischith T R <nischith.tr@gmail.com>
2020-09-19 15:16:34 +0530

    QAM modulator according to standards - Constellation Mapper
    creverb1 made a function
    Constellation mapper takes care of odd b_n as well
    test calls creverb and qam_mod

--- creverb1.m
+++ creverb1.m
@@ -1,6 +1,6 @@
-clc;
-nsc=7; % Number of Sub Channels
-for i=1:1:4*nsc
-    rand_num = psbr(i, nsc);
-    disp(i+"="+rand_num)
+function [rand_num] = creverb1(nsc)
+    rand_num = [];
+    for i=1:1:4*nsc
+        rand_num = [rand_num psbr(i, nsc)];
+    end
 end
