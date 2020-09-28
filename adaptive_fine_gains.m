507c7c39824a99e2d3026e546f9dd7c3d554a3c3
Nagendra-KJ <nagendrajamadagni@gmail.com>
2020-09-28 12:55:12 +0530

    Fixed issue with nbits < 0 and nbits > bi_max

--- adaptive_fine_gains.m
+++ adaptive_fine_gains.m
@@ -1,4 +1,5 @@
 function optimized_channels=adaptive_fine_gains(snr, b_target)
+bi_max=20; % Maximum number of bits allowed per channel
 snr_abs = 10.^(snr./10); % From given snr array turn it into absolute snr array
 unoptimized_channels = channel_capacity(snr_abs, b_target); %Find a rough optimization through the algorithm
 b_total=sum([unoptimized_channels.nbits_rounded]); %Find total number of bits
@@ -8,17 +9,25 @@ if(b_total==0) %If total number of bits is 0, then bad channel
 end
 disp("Maximum channel capacity after applying Shannon's Law at existing conditions is " + b_total);
 while(b_total>b_target) %If b_total>b_target, remove one bit from channel with smallest delta and recalculate delta for that channel
+k=1;
 [~, ind] = sort([unoptimized_channels.diff],'ascend');
 unoptimized_channels = unoptimized_channels(ind);
-unoptimized_channels(1).nbits_rounded=unoptimized_channels(1).nbits_rounded-1;
-unoptimized_channels(1).find_diff();
+while(unoptimized_channels(k).nbits_rounded==0)
+    k=k+1;
+end
+unoptimized_channels(k).nbits_rounded=unoptimized_channels(k).nbits_rounded-1;
+unoptimized_channels(k).find_diff();
 b_total=b_total-1;
 end
 while(b_total<b_target) %If b_total<b_target, add one bit from channel with smallest delta and recalculate delta for that channel
+k=1;
 [~, ind] = sort([unoptimized_channels.diff],'descend');
 unoptimized_channels = unoptimized_channels(ind);
-unoptimized_channels(1).nbits_rounded=unoptimized_channels(1).nbits_rounded+1;
-unoptimized_channels(1).find_diff();
+while(unoptimized_channels(k).nbits_rounded>bi_max)
+    k=k+1;
+end
+unoptimized_channels(k).nbits_rounded=unoptimized_channels(k).nbits_rounded+1;
+unoptimized_channels(k).find_diff();
 b_total=b_total+1;
 end
 optimized_channels=unoptimized_channels; %Now b_total=b_target so recalculate the snrs for the channel
