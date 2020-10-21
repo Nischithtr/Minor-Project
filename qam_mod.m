3c3558e35328f3f6466b743dbc7fa8aa595bd011
Nischith T R <nischith.tr@gmail.com>
2020-10-21 13:34:26 +0530

    gain scaling factor multiplied with symbols

--- qam_mod.m
+++ qam_mod.m
@@ -8,13 +8,16 @@ function [mapped_val] = qam_mod(varargin)
     % t is tone allocation table
     t = varargin{3};
     
+    % g_array is gain scaling factor
+    g_array = varargin{4};
+    
     % If trellis coding is to be done  do tone_ordering
     % Otherwise pass the same b and t
     % Peroform bit extraction
     % Trellis coding is incomplete
        
-    if(nargin == 4)
-        if(strcmp(varargin{4} , 'trellis'))
+    if(nargin == 5)
+        if(strcmp(varargin{5} , 'trellis'))
             [b_n t_n] = tone_order(b,t) 
             extracted_bits = bit_extract(data,b_n,'trellis');
         else
@@ -25,5 +28,5 @@ function [mapped_val] = qam_mod(varargin)
         t_n = t;
         extracted_bits = bit_extract(data,b_n); % Extract bits onto the variable based on bit allocation table
     end
-    mapped_val = constellation_mapper(extracted_bits,b_n); % Convert the extracted bits to QAM symbols
+    mapped_val = g_array .* constellation_mapper(extracted_bits,b_n); % Convert the extracted bits to QAM symbols
 end
--- test.m
+++ test.m
@@ -1,12 +1,13 @@
 t=1:256; % Tone loading table not yet used.
 snr=SPN_to_SNR_converter(); % Get the SNR table
 b=basic_fine_gains(snr); % Get the adaptive bit loading
-g_array=[b.pdiff];
-disp(g_array)
+g_array=[b.pdiff]' ;
 b=[b.nbits_rounded]; % Get only the number of rounded bits in the channel
 [~,Nsc] = size(b); % Number of sub-carriers
+sum(b)
 data = creverb1(Nsc); % PRBS
-QAM_symbols = qam_mod(data,b,t); % QAM symbols
+data = [data  randi([0,1],1,635)] ;
+QAM_symbols = qam_mod(data,b,t,g_array); % QAM symbols
 
 noise_power_db = -140 : 10 : 30; % Noise power in db
 noise_power_abs = 10 .^ (noise_power_db ./ 10); % Absolute noise power
@@ -20,8 +21,8 @@ received_symbols =  QAM_symbols + noise; % QAM symbol + AWGN noise
 data = data(1:sum(b)); % Get the BER
 
 for ii = 1 : 18
-    bit_stream_rcvd(ii,:) = qam_demod(received_symbols(:,ii),b); % Perform demod on the receiver end
-    err(ii) = sum(bit_stream_rcvd(ii,:) ~= data) / sum(b)
+    bit_stream_rcvd(ii,:) = qam_demod(received_symbols(:,ii),b,g_array); % Perform demod on the receiver end
+    err(ii) = sum(bit_stream_rcvd(ii,:) ~= data) / sum(b);
 end
 
 % Plot of BER vs SNR
